25685d67f009c024e414877993e3beb88072023f
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-21 10:52:19 +0600

    lec-53 post method in Rest API

--- src/App.js
+++ src/App.js
@@ -99,42 +99,83 @@
 
 // Lec-51 start fetch API | Get method
 
-import "./App.css";
-import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import React, { useEffect, useState } from "react";
+// function App() {
+//   const [users, setUser] = useState([]);
+//   useEffect(() => {
+//     fetch("http://localhost:4000/todo").then((result) => {
+//       result.json().then((resp) => {
+//         // console.warn(resp)
+//         setUser(resp);
+//       });
+//     });
+//   }, []);
+//   console.warn(users);
+//   return (
+//     <div className="App">
+//       <h1>Get API Call </h1>
+//       <table border="1">
+//         <tbody>
+//           <tr>
+//             <td>ID</td>
+//             <td>Name</td>
+//             <td>Email</td>
+//             <td>Mobile</td>
+//           </tr>
+//           {users.map((item, i) => (
+//             <tr key={i}>
+//               <td>{item.userId}</td>
+//               <td>{item.name}</td>
+//               <td>{item.email}</td>
+//               <td>{item.mobile}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+// export default App;
+
+// Lec-51 End fetch API | Get method
+
+// Lec-53 start Post method in Rest API
+
+
+import './App.css';
+import React, { useEffect, useState } from 'react'
 function App() {
-  const [users, setUser] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:4000/todo").then((result) => {
-      result.json().then((resp) => {
-        // console.warn(resp)
-        setUser(resp);
-      });
-    });
-  }, []);
-  console.warn(users);
+ const [name,setName]=useState("");
+ const [email,setEmail]=useState("");
+ const [mobile,setMobile]=useState("");
+function saveData()
+{
+  let data={name,email,mobile}
+// console.warn(data);
+  fetch("http://localhost:4000/todo", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(data)
+  }).then((resp)=>{
+    // console.warn("resp",resp);;
+    resp.json().then((result)=>{
+      console.warn("result",result)
+    })
+  })
+}
   return (
     <div className="App">
-      <h1>Get API Call </h1>
-      <table border="1">
-        <tbody>
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Mobile</td>
-          </tr>
-          {users.map((item, i) => (
-            <tr key={i}>
-              <td>{item.userId}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.mobile}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1>POST API Example </h1>  
+      <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}}  /> <br /> <br />
+      <input type="text" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /> <br />
+      <input type="text" name="mobile"  value={mobile} onChange={(e)=>{setMobile(e.target.value)}}/> <br /> <br />
+      <button type="button" onClick={saveData} >Save New User</button>
     </div>
   );
 }
 export default App;
-// Lec-51 End fetch API | Get method
+// Lec-53 end Post method in Rest API
